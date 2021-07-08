3b5b82c4f2531fcc4ad2843b6ea36018592ebcc6
alexbashkatov6 <alexbashkatov6@gmail.com>
2021-07-08 12:29:00 +0300

    Begin of project

--- main.py
+++ main.py
@@ -1,50 +1,78 @@
+
 import sys
-from PyQt5 import QtCore
-from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication
+from PyQt5.QtGui import QIcon
 
 
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog,QGraphicsView,QGraphicsScene,QVBoxLayout
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
 
 
+    def initUI(self):
 
-from PyQt5.QtWidgets import (QApplication, QLabel, QWidget)
-from PyQt5.QtGui import QPainter, QColor, QPen
-from PyQt5.QtCore import Qt, QPoint, QSize
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAction = QAction(QIcon('pictures/01_CS.jpg'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAction)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main window')
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+'''
+import sys
+from PyQt5.QtWidgets import QLabel, QWidget, QVBoxLayout, QApplication
+from PyQt5.QtCore import QPoint, QSize
 from PyQt5.QtGui import QPainter, QPainterPath
-from PyQt5.QtCore import pyqtSignal, QObject
-from PyQt5.QtWidgets import QMainWindow, QApplication
-
+from PyQt5.QtCore import pyqtSignal
 
 
 class Drawer(QWidget):
     newPoint = pyqtSignal(QPoint)
+
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.path = QPainterPath()
-
 
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.drawPath(self.path)
 
-
-
     def mousePressEvent(self, event):
         self.path.moveTo(event.pos())
         self.update()
-
-
 
     def mouseMoveEvent(self, event):
         self.path.lineTo(event.pos())
         self.newPoint.emit(event.pos())
         self.update()
 
-
-
     def sizeHint(self):
         return QSize(400, 400)
-
 
 
 class MyWidget(QWidget):
@@ -58,9 +86,9 @@ class MyWidget(QWidget):
         self.layout().addWidget(drawer)
 
 
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = MyWidget()
     w.show()
     sys.exit(app.exec_())
+'''
