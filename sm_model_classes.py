5b45190499a58fb81d02adeb1e0cac0df1381d48
alexbashkatov6 <alexbashkatov6@gmail.com>
2021-07-21 17:51:37 +0300

    Comment part of BFP + block on 'Classname_'

--- sm_model_classes.py
+++ sm_model_classes.py
@@ -8,6 +8,7 @@ class BFP:
         if 'Name' in kwargs.keys():
             name_pretend = kwargs['Name']
             prefix = self.__class__.__name__ + '_'
+            assert name_pretend != prefix, 'Auto-name cannot be == prefix; add specification to end'
             assert not name_pretend[len(prefix):].isdigit(), 'Not auto-name cannot be (prefix + int); choose other name'
             self.Name = kwargs['Name']
         else:
@@ -30,27 +31,27 @@ class BFP:
         assert not Oi.checkNameRepeating(self.__class__, name_pretend), 'Name {} is already exists'.format(name_pretend)
         self._Name = name_pretend
 
-    def strInitialization(self, new_attribs_dict):
-        curr_attribs_dict = self.__dict__
-        for attr_name, attr_new_value in new_attribs_dict.items():
-            if attr_name == 'Name':
-                continue
-            assert ('_' + attr_name) in curr_attribs_dict.keys(), 'Attrib {} not found'.format(attr_name)
-            if type(attr_new_value) == str:
-                setattr(self, '_' + attr_name, attr_new_value)
-            else:
-                setattr(self, attr_name, attr_new_value)
-
-    def strEvaluation(self):
-        # automatically works only for properties... thats enough ?
-        cls_ierarh = reversed(self.__class__.__mro__[:-1])
-        for cls in cls_ierarh:
-            cls_prop_names = list(filter(lambda i: type(cls.__dict__[i]) == property, cls.__dict__.keys()))
-            for prop_name in cls_prop_names:
-                if prop_name == 'Name':
-                    continue
-                assert '_' + prop_name in self.__dict__.keys(), 'Prop {} not in obj'.format(prop_name)
-                setattr(self, prop_name, getattr(self, '_' + prop_name))
+    # def strInitialization(self, new_attribs_dict):
+    #     curr_attribs_dict = self.__dict__
+    #     for attr_name, attr_new_value in new_attribs_dict.items():
+    #         if attr_name == 'Name':
+    #             continue
+    #         assert ('_' + attr_name) in curr_attribs_dict.keys(), 'Attrib {} not found'.format(attr_name)
+    #         if type(attr_new_value) == str:
+    #             setattr(self, '_' + attr_name, attr_new_value)
+    #         else:
+    #             setattr(self, attr_name, attr_new_value)
+    #
+    # def strEvaluation(self):
+    #     # automatically works only for properties... thats enough ?
+    #     cls_ierarh = reversed(self.__class__.__mro__[:-1])
+    #     for cls in cls_ierarh:
+    #         cls_prop_names = list(filter(lambda i: type(cls.__dict__[i]) == property, cls.__dict__.keys()))
+    #         for prop_name in cls_prop_names:
+    #             if prop_name == 'Name':
+    #                 continue
+    #             assert '_' + prop_name in self.__dict__.keys(), 'Prop {} not in obj'.format(prop_name)
+    #             setattr(self, prop_name, getattr(self, '_' + prop_name))
 
     @classmethod
     def allSubclasses(cls):
@@ -75,6 +76,8 @@ class Line(BFP):
 class GroundLine(BFP):
     pass
 
-pnt1, pnt2 = Point(), Point(Name='Point_tri')
-print(pnt2.__dict__)
+
+pnt1, pnt2, pnt3 = Point(), Point(Name='Point_'), Point(Name='Point_my2')
+print(pnt3.__dict__)
 print(BFP.allSubclasses())
+print(Oi.getInstances(Point))
