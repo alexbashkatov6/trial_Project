7b68ce7fd040569e1efcbedb6ccef4cba65bf115
alexbashkatov6 <alexbashkatov6@gmail.com>
2021-12-14 17:50:32 +0300

    New objects handlers begin 2

--- core_object_handler.py
+++ core_object_handler.py
@@ -10,6 +10,7 @@ name
 
 CoordinateSystem
 cs_relative_to
+dependence 'dependent' 'independent'
 x
 y
 alpha
@@ -26,6 +27,7 @@ alpha
 
 Point
 cs_relative_to
+on_line_or_ground_line
 x
 ground_line
 line
@@ -36,12 +38,53 @@ second_point
 '''
 
 
+class EvaluationCS:
+    def __init__(self, base_cs: EvaluationCS = None, co_x: bool = True, x: int = 0):
+        self._base_cs = base_cs
+        self._is_base = base_cs is None
+        self._in_base_x = x
+        self._in_base_co_x = co_x
+
+        self._absolute_x = 0
+        self._absolute_co_x = True
+        self.eval_absolute_parameters()
+
+    @property
+    def is_base(self):
+        return self._is_base
+
+    @property
+    def base_cs(self):
+        return self._base_cs
+
+    @property
+    def in_base_x(self):
+        return self._in_base_x
+
+    @property
+    def in_base_co_x(self):
+        return self._in_base_co_x
+
+    @property
+    def absolute_x(self) -> int:
+        return self._absolute_x
+
+    @property
+    def absolute_co_x(self) -> bool:
+        return self._absolute_co_x
+
+    def eval_absolute_parameters(self):
+        if not self.is_base:
+            self._absolute_x = self.base_cs.absolute_x + self.in_base_x
+            self._absolute_co_x = self.base_cs.absolute_co_x == self.in_base_co_x
+
+
 class CoreObjectHandler(QObject):
     obj_created = pyqtSignal(dict)
 
     def __init__(self):
         super().__init__()
-        pass
+        self.cs_dict = {}
 
     @pyqtSlot(dict)
     def got_obj_created(self, obj_info: dict):
@@ -57,18 +100,19 @@ class CoreObjectHandler(QObject):
         elif isinstance(obj, Line):
             self.new_line_handler(obj)
 
-    def new_cs_handler(self, obj):
-        print("in new_cs_handler")
-        for attr in obj.__dict__:
-            print("attr", attr)
-        print('cox', type(obj.co_x))
+    def obj_changed(self, obj_info: dict):
+        pass
 
-    def new_ground_line_handler(self, obj):
+    def new_cs_handler(self, cs):
+        print("in new_cs_handler")
+        if cs.dependence == 'dependent':
+            eCS = EvaluationCS()
+
+    def new_ground_line_handler(self, gl):
         print("in new_ground_line_handler")
 
-    def new_point_handler(self, obj):
+    def new_point_handler(self, pnt):
         print("in new_point_handler")
 
-    def new_line_handler(self, obj):
+    def new_line_handler(self, ln):
         print("in new_line_handler")
-
