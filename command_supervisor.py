da9f39e9348b05123febc6b9ea848b737f2fa140
alexbashkatov6 <alexbashkatov6@gmail.com>
2021-12-16 09:49:28 +0300

    Images storage begin

--- command_supervisor.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, QObject
-
-
-class CommandSupervisor(QObject):
-    def __init__(self):
-        super(CommandSupervisor, self).__init__()
