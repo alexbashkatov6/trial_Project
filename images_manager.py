e63ca822a80072949cc2e81e7c846230ad1cb2e4
alexbashkatov6 <alexbashkatov6@gmail.com>
2021-12-17 09:43:25 +0300

    6-classes images manager

---
+++ images_manager.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+from collections import OrderedDict
+
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QObject
+
+from nv_config import CLASSES_SEQUENCE
+
+
+class ImgBuilder:
+    def __init__(self, manager: ImagesManager):
+        self.mng = manager
+
+    def copy_obj(self):
+        pass
+
+
+class ImgValidator:
+    def __init__(self, manager: ImagesManager):
+        self.mng = manager
+
+    def validate_value(self):
+        pass
+
+
+class ImgCommandSupervisor:
+    def __init__(self, manager: ImagesManager):
+        self.mng = manager
+        self.redo_commands = []
+        self.undo_commands = []
+
+
+class ImgDependenceGraph:
+    def __init__(self, manager: ImagesManager):
+        self.mng = manager
+        self.clear_dg = None
+
+    def virtual_builtin(self):
+        pass
+
+    def linearize(self):
+        pass
+
+    def validate_img(self):
+        pass
+
+    def read_from_file(self):
+        pass
+
+    def dump_to_file(self):
+        pass
+
+
+class ImgTreeView(QObject):
+    def __init__(self, manager: ImagesManager):
+        super().__init__()
+        self.mng = manager
+
+
+class ImgAttribView(QObject):
+    def __init__(self, manager: ImagesManager):
+        super().__init__()
+        self.mng = manager
+
+
+class ImagesManager:
+    def __init__(self):
+        self.tree_view = ImgBuilder(self)
+        self.attrib_view = ImgAttribView(self)
+
+        self.builder = ImgBuilder(self)
+        self.validator = ImgValidator(self)
+        self.dg = ImgDependenceGraph(self)
+        self.comm_sup = ImgCommandSupervisor(self)
