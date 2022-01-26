dc79e05b8fd59823b5ab3862375dba5f7bd01b34
alexbashkatov6 <alexbashkatov6@gmail.com>
2022-01-26 09:52:59 +0300

    Add route classes

--- station_object_images.py
+++ station_object_images.py
@@ -7,6 +7,7 @@ import os
 from abc import abstractmethod
 from numbers import Real
 import math
+import re
 
 from custom_enum import CustomEnum
 from two_sided_graph import OneComponentTwoSidedPG, PolarNode, Element
@@ -634,35 +635,34 @@ def get_object_by_name(name, obj_list) -> StationObjectImage:
 # -------------------------        CELLS           -------------------- #
 
 
-class RailPointDirectionCell(CellObject):
-    def __init__(self, direction: str):
-        self.direction = direction
-
-
 class PointCell(CellObject):
-    def __init__(self, name: str, point: PointMO):
+    def __init__(self, name: str):
         self.name = name
-        self.point = point
 
 
 class LightCell(CellObject):
-    def __init__(self, light: LightMO):
-        self.light = light
-
-
-class LineCell(CellObject):
-    def __init__(self, line: LineMO):
-        self.line = line
+    def __init__(self, name: str):
+        self.name = name
 
 
 class IsolatedSectionCell(CellObject):
-    def __init__(self, section_name: str):
-        self.section_name = section_name
+    def __init__(self, name: str):
+        self.name = name
+
+
+class LineCell(CellObject):
+    def __init__(self, name: str):
+        self.name = name
 
 
 class LengthCell(CellObject):
     def __init__(self, length: float):
         self.length = length
+
+
+class RailPointDirectionCell(CellObject):
+    def __init__(self, direction: str):
+        self.direction = direction
 
 
 class CellError(Exception):
@@ -860,6 +860,346 @@ GLOBAL_CS_SO = CoordinateSystemSOI()
 GLOBAL_CS_SO._name = GLOBAL_CS_NAME
 GLOBAL_CS_MO = CoordinateSystemMO()
 GLOBAL_CS_MO._name = GLOBAL_CS_NAME
+
+
+# -------------------------        ROUTE CLASSES           -------------------- #
+
+
+class CrossroadNotification:
+    def __init__(self, cn_route: Route, num: int):
+        self.route = cn_route
+        self.num = num
+        self._crsrd_id = None
+        self._crsrd_delay_open = None
+        self._crsrd_delay_start_notif = None
+        self._crsrd_start_notif = None
+        self._crsrd_notif_point = None  # not required
+        self._crsrd_before_route_points = None  # not required
+
+    @property
+    def crsrd_id(self):
+        return self._crsrd_id
+
+    @crsrd_id.setter
+    def crsrd_id(self, value):
+        if (not value) or value.isspace():
+            return
+        # self.route.int_checker(value, 'crsrd_id_{}'.format(self.num))
+        self._crsrd_id = value
+
+    @property
+    def crsrd_delay_open(self):
+        return self._crsrd_delay_open
+
+    @crsrd_delay_open.setter
+    def crsrd_delay_open(self, value):
+        if (not value) or value.isspace():
+            return
+        # assert value and not value.isspace(), "Empty crsrd_delay_open_{} in line {}". \
+        #     format(self.num, self.route.line_in_excel)
+        self.route.int_checker(value, 'crsrd_delay_open_{}'.format(self.num), 0)
+        self._crsrd_delay_open = value
+
+    @property
+    def crsrd_delay_start_notif(self):
+        return self._crsrd_delay_start_notif
+
+    @crsrd_delay_start_notif.setter
+    def crsrd_delay_start_notif(self, value):
+        if (not value) or value.isspace():
+            return
+        # assert value and not value.isspace(), "Empty crsrd_delay_start_notif_{} in line {}". \
+        #     format(self.num, self.route.line_in_excel)
+        self.route.int_checker(value, 'crsrd_delay_start_notif_{}'.format(self.num), 0)
+        self._crsrd_delay_start_notif = value
+
+    @property
+    def crsrd_start_notif(self):
+        return self._crsrd_start_notif
+
+    @crsrd_start_notif.setter
+    def crsrd_start_notif(self, value):
+        if (not value) or value.isspace():
+            return
+        # assert value and not value.isspace(), "Empty crsrd_start_notif_{} in line {}". \
+        #     format(self.num, self.route.line_in_excel)
+        # ! implement here check start_notif in list of available values
+        self._crsrd_start_notif = value
+
+    @property
+    def crsrd_notif_point(self):
+        return self._crsrd_notif_point
+
+    @crsrd_notif_point.setter
+    def crsrd_notif_point(self, value):
+        if (not value) or value.isspace():
+            return
+        self.route.int_checker(value, 'crsrd_notif_point_{}'.format(self.num))
+        self._crsrd_notif_point = value
+
+    @property
+    def crsrd_before_route_points(self):
+        return self._crsrd_before_route_points
+
+    @crsrd_before_route_points.setter
+    def crsrd_before_route_points(self, value):
+        if (not value) or value.isspace():
+            return
+        self.route.route_points_checker(value, 'crsrd_before_route_points_{}'.format(self.num))
+        self._crsrd_before_route_points = value
+
+    def check_required_params(self):
+        if self.route.signal_type == "PpoTrainSignal":
+            if self.crsrd_id is None:
+                assert (self.crsrd_delay_open is None) and (self.crsrd_delay_start_notif is None) and \
+                       (self.crsrd_start_notif is None) and (self.crsrd_notif_point is None) and \
+                       (self.crsrd_before_route_points is None), \
+                       "Id expected for Crossroad_{} in line {}".format(self.num, self.route.id)
+            else:
+                assert not (self.crsrd_delay_open is None), "Expected delay_open for Crossroad_{} in line {}".\
+                    format(self.num, self.route.id)
+                assert not (self.crsrd_delay_start_notif is None), \
+                    "Expected delay_start_notif for Crossroad_{} in line {}".format(self.num, self.route.id)
+                assert not (self.crsrd_start_notif is None), "Expected start_notif for Crossroad_{} in line {}".\
+                    format(self.num, self.route.id)
+        elif self.route.signal_type == "PpoShuntingSignal":
+            if self.crsrd_id is None:
+                assert (self.crsrd_delay_open is None) and (self.crsrd_delay_start_notif is None) and \
+                       (self.crsrd_start_notif is None) and (self.crsrd_notif_point is None) and \
+                       (self.crsrd_before_route_points is None), \
+                       "Id expected for Crossroad_{} in line {}".format(self.num, self.route.id)
+            else:
+                assert not (self.crsrd_delay_open is None), "Expected delay_open for Crossroad_{} in line {}".\
+                    format(self.num, self.route.id)
+        else:
+            assert False, "Signal type {} not exists".format(self.route.signal_type)
+
+
+class Route:
+    def __init__(self, id_):
+        self.id = str(id_)
+        self.route_tag = None
+        self._route_type = None
+        self._signal_tag = None
+        self._signal_type = None
+        self._route_pointer_value = None
+        self._trace_begin = None
+        self.trace_points = ""
+        self._trace_variants = None
+        self._trace_end = None
+        self._end_selectors = None
+        self._route_points_before_route = None
+        self.next_dark = "K"
+        self.next_stop = "K"
+        self.next_on_main = "K"
+        self.next_on_main_green = "K"
+        self.next_on_side = "K"
+        self.next_also_on_main = "K"
+        self.next_also_on_main_green = "K"
+        self.next_also_on_side = "K"
+        self.crossroad_notifications: list[CrossroadNotification] = []
+
+    def signal_light_checker(self, value, column_name):
+        if self.route_type == "PpoShuntingRoute":
+            return
+        assert value in ["K", "ZH", "Z", "ZHM_Z", "ZHM_ZH", "ZM", "DZH", "DZHM"], \
+            "Not supported light value {} in line {} column {}".format(value, self.id, column_name)
+
+    def int_checker(self, value, column_name, min_possible_value: int = 1):
+        if value == "":
+            return
+        assert int(value) >= min_possible_value, "Value should be int >= {}, given value is {} in line {} column {}" \
+            .format(min_possible_value, value, self.id, column_name)
+
+    def route_points_checker(self, value, column_name):
+        points_found = re.findall(r"[+-]\d{1,3}S?[OB]?", value)
+        val_copy = value
+        for point in points_found:
+            val_copy = val_copy.replace(point, "", 1)
+        assert (not val_copy) or val_copy.isspace(), \
+            "Pointers list {} is not valid in line {} column {}".format(value, self.id, column_name)
+
+    @property
+    def route_type(self):
+        return self._route_type
+
+    @route_type.setter
+    def route_type(self, value):
+        assert value in ["PpoTrainRoute", "PpoShuntingRoute"], "Not valid route type {} in line {}" \
+            .format(value, self.id)
+        self._route_type = value
+
+    @property
+    def signal_tag(self):
+        return self._signal_tag
+
+    @signal_tag.setter
+    def signal_tag(self, value):
+        # ! implement here check signal in list of available values
+        self._signal_tag = value
+
+    @property
+    def signal_type(self):
+        return self._signal_type
+
+    @signal_type.setter
+    def signal_type(self, value):
+        assert value in ["PpoTrainSignal", "PpoShuntingSignal"], "Not valid signal type {} in line {}" \
+            .format(value, self.id)
+        self._signal_type = value
+
+    @property
+    def route_pointer_value(self):
+        return self._route_pointer_value
+
+    @route_pointer_value.setter
+    def route_pointer_value(self, value):
+        self.int_checker(value, 'route_pointer_value')
+        self._route_pointer_value = value
+
+    @property
+    def trace_begin(self):
+        return self._trace_begin
+
+    @trace_begin.setter
+    def trace_begin(self, value):
+        # ! implement here check trace_begin in list of available values
+        self._trace_begin = value
+
+    @property
+    def trace_variants(self):
+        return self._trace_variants
+
+    @trace_variants.setter
+    def trace_variants(self, value):
+        if value == "":
+            self._trace_variants = None
+            return
+        # ! implement here check trace_variants in list of available values
+        self._trace_variants = value
+
+    @property
+    def trace_points(self):
+        return self._trace_points
+
+    @trace_points.setter
+    def trace_points(self, value: str):
+        self.route_points_checker(value, 'trace_points')
+        if value:
+            value += " "
+        self._trace_points = value
+
+    @property
+    def trace_end(self):
+        return self._trace_end
+
+    @trace_end.setter
+    def trace_end(self, value):
+        # ! implement here check trace_end in list of available values
+        self._trace_end = value
+
+    @property
+    def end_selectors(self):
+        return self._end_selectors
+
+    @end_selectors.setter
+    def end_selectors(self, value):
+        # ! implement here check end_selectors in list of available values
+        self._end_selectors = value
+
+    @property
+    def route_points_before_route(self):
+        return self._route_points_before_route
+
+    @route_points_before_route.setter
+    def route_points_before_route(self, value):
+        if value == "":
+            self._route_points_before_route = None
+            return
+        # ! implement here check route_points_before_route in list of available values
+        self._route_points_before_route = value + " "
+
+    @property
+    def next_dark(self):
+        return self._next_dark
+
+    @next_dark.setter
+    def next_dark(self, value):
+        self.signal_light_checker(value, "next_dark")
+        self._next_dark = value
+
+    @property
+    def next_stop(self):
+        return self._next_stop
+
+    @next_stop.setter
+    def next_stop(self, value):
+        self.signal_light_checker(value, "next_stop")
+        self._next_stop = value
+
+    @property
+    def next_on_main(self):
+        return self._next_on_main
+
+    @next_on_main.setter
+    def next_on_main(self, value):
+        self.signal_light_checker(value, "next_on_main")
+        self._next_on_main = value
+
+    @property
+    def next_on_main_green(self):
+        return self._next_on_main_green
+
+    @next_on_main_green.setter
+    def next_on_main_green(self, value):
+        self.signal_light_checker(value, "next_on_main_green")
+        self._next_on_main_green = value
+
+    @property
+    def next_on_side(self):
+        return self._next_on_side
+
+    @next_on_side.setter
+    def next_on_side(self, value):
+        self.signal_light_checker(value, "next_on_side")
+        self._next_on_side = value
+
+    @property
+    def next_also_on_main(self):
+        return self._next_also_on_main
+
+    @next_also_on_main.setter
+    def next_also_on_main(self, value):
+        self.signal_light_checker(value, "next_also_on_main")
+        self._next_also_on_main = value
+
+    @property
+    def next_also_on_main_green(self):
+        return self._next_also_on_main_green
+
+    @next_also_on_main_green.setter
+    def next_also_on_main_green(self, value):
+        self.signal_light_checker(value, "next_also_on_main_green")
+        self._next_also_on_main_green = value
+
+    @property
+    def next_also_on_side(self):
+        return self._next_also_on_side
+
+    @next_also_on_side.setter
+    def next_also_on_side(self, value):
+        self.signal_light_checker(value, "next_also_on_side")
+        self._next_also_on_side = value
+
+    def count_crossroad_notification(self):
+        return len(self.crossroad_notifications)
+
+    def add_crossroad_notification(self):
+        cn = CrossroadNotification(self, self.count_crossroad_notification() + 1)
+        self.crossroad_notifications.append(cn)
+
+
+# ------------------------------------------------------
 
 
 def check_expected_type(str_value: str, attr_name: str, image_object: StationObjectImage, names_dict: OrderedDict):
@@ -1170,7 +1510,7 @@ class ModelProcessor:
                         except ParallelLinesException:
                             continue
                         except EquivalentLinesException:
-                            raise BuildSkeletonError("Cannot re-build existing axis")  # "Cannot re-build existing axis"
+                            raise BuildSkeletonError("Cannot re-build existing axis")
 
                 if image.creation_method == CEAxisCreationMethod.rotational:
                     center_point_soi: PointSOI = image.center_point
@@ -1268,7 +1608,7 @@ class ModelProcessor:
         next_node: PolarNode = single_element(lambda node: get_cell_by_class(node, "PointCell").name == next_point.name,
                                               self.smg.not_inf_nodes)
         new_point_node = self.smg.insert_node(next_node.ni_nd, prev_node.ni_pu)
-        new_point_node.append_cell_obj(PointCell(point.name, point))
+        new_point_node.append_cell_obj(PointCell(point.name))
 
         line.append_point(point)
 
@@ -1290,7 +1630,7 @@ class ModelProcessor:
                                                  self.smg.not_inf_nodes)
         except EINotFoundError:
             min_node = self.smg.insert_node()
-            min_node.append_cell_obj(PointCell(min_point.name, min_point))
+            min_node.append_cell_obj(PointCell(min_point.name))
 
         try:
             max_node: PolarNode = single_element(lambda node:
@@ -1298,7 +1638,7 @@ class ModelProcessor:
                                                  self.smg.not_inf_nodes)
         except EINotFoundError:
             max_node = self.smg.insert_node()
-            max_node.append_cell_obj(PointCell(max_point.name, max_point))
+            max_node.append_cell_obj(PointCell(max_point.name))
 
         self.smg.connect_inf_handling(min_node.ni_pu, max_node.ni_nd)
 
@@ -1320,7 +1660,7 @@ class ModelProcessor:
                 self.smg.connect_inf_handling(last_nd_interface, point_node.ni_pu)
             except EINotFoundError:
                 point_node = self.smg.insert_node(last_nd_interface)
-                point_node.append_cell_obj(PointCell(line_point.name, line_point))
+                point_node.append_cell_obj(PointCell(line_point.name))
             last_nd_interface = point_node.ni_nd
 
         axis.append_line(line)
@@ -1330,7 +1670,8 @@ class ModelProcessor:
         for link in self.smg.not_inf_links:
             pn_s_ = [ni.pn for ni in link.ni_s]
             pnt_cells_: list[PointCell] = [get_cell_by_class(pn, "PointCell") for pn in pn_s_]
-            link.append_cell_obj(LengthCell(abs(pnt_cells_[0].point.x - pnt_cells_[1].point.x)))
+            link.append_cell_obj(LengthCell(abs(self.names_mo[pnt_cells_[0].name].x -
+                                                self.names_mo[pnt_cells_[1].name].x)))
 
     def build_equipment(self):
 
@@ -1357,7 +1698,7 @@ class ModelProcessor:
 
                 model_object = LightMO(image.light_route_type, center_point, direct_point,
                                        image.colors, image.light_stick_type)
-                center_point_node.append_cell_obj(LightCell(model_object))
+                center_point_node.append_cell_obj(LightCell(model_object.name))
 
                 model_object.name = image_name
                 self.names_mo[image_name] = model_object
@@ -1621,4 +1962,4 @@ if __name__ == "__main__":
                 if move.cell_objs:
                     rpdc = get_cell_by_class(move, "RailPointDirectionCell")
                     print("Rail point direction = ", rpdc.direction)
-            print("section {}".format(get_cell_by_class(link, "IsolatedSectionCell").section_name))
+            print("section {}".format(get_cell_by_class(link, "IsolatedSectionCell").name))
